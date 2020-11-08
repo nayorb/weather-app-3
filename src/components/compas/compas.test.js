0c2d45e55c51762f5f25a95f73cd27f889075672
nayorb <36243913+nayorb@users.noreply.github.com>
2020-11-08 17:17:37 +0100

    compas tests

---
+++ src/components/compas/compas.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Compas from "./compas";
+import { fireEvent, render } from "@testing-library/react";
+
+describe("DurationTabs (component)", () => {
+  test("renders", () => {
+    render(<Compas />);
+  });
+
+  test("renders with proper angle", () => {
+    const testAngle = 137;
+    const container = render(<Compas angle={testAngle} />);
+
+    const rotationContainer = container.getByTestId(
+      "compas-rotation-container"
+    );
+    expect(rotationContainer).toHaveStyle("transform: rotate(317deg)");
+  });
+});
